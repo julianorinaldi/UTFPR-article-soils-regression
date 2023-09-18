7625753887073876eddde33250d324dd9a56a51c
julianorinaldi <julianorinaldi@gmail.com>
2023-09-18 09:15:47 -0300

    Outra estratégia 3

--- training_02_carbon_parallel.py
+++ training_02_carbon_parallel.py
@@ -120,7 +120,8 @@ print(f'Shape Y_test_carbono: {Y_test_carbono.shape}')
 #Y_test_nitrogenio = np.array(df_test['teor_nitrogenio'].tolist()[:qtd_imagens])
 #print(f'Shape Y_test_nitrogenio: {Y_test_nitrogenio.shape}')
 
-strategy = tf.distribute.MirroredStrategy()
+strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"])
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
 with strategy.scope():
     resnet_model = tf.keras.models.Sequential()
