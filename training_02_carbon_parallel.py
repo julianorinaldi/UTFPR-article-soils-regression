6bb74c1a71b52b6e9f359a6f3460bdd0496712a8
julianorinaldi <julianorinaldi@gmail.com>
2023-09-18 08:59:50 -0300

    tf.distribute.MirroredStrategy()

--- training_02_carbon_parallel.py
+++ training_02_carbon_parallel.py
@@ -119,7 +119,7 @@ print(f'Shape Y_test_carbono: {Y_test_carbono.shape}')
 #Y_test_nitrogenio = np.array(df_test['teor_nitrogenio'].tolist()[:qtd_imagens])
 #print(f'Shape Y_test_nitrogenio: {Y_test_nitrogenio.shape}')
 
-strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"])
+strategy = tf.distribute.MirroredStrategy()
 
 with strategy.scope():
     resnet_model = tf.keras.models.Sequential()
