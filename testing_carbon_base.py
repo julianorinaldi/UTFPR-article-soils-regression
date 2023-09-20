3e9135dc6b695928d4e9a8b51e4d0ad660e496e4
julianorinaldi <julianorinaldi@gmail.com>
2023-09-20 12:18:33 -0300

    Teste

--- testing_carbon_base.py
+++ testing_carbon_base.py
@@ -55,7 +55,7 @@ if (args.debug):
     print(f'{prefix} Amount of GPU Available: {physical_devices}')
 
 # Estratégia para trabalhar com Multi-GPU
-strategy = tf.distribute.MirroredStrategy(devices=["GPU:1"],
+strategy = tf.distribute.MirroredStrategy(devices=['/gpu:1'],
     cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
 
 with strategy.scope():
