c2f28b6de40e021cc0c322bf14c9b14f8c7f77df
julianorinaldi <julianorinaldi@gmail.com>
2023-09-25 22:30:04 -0300

    Improve

--- X-SoilRegression.py
+++ X-SoilRegression.py
@@ -23,8 +23,8 @@ parser.add_argument("-n", "--name", help="Nome do arquivo de saída do modelo .t
 parser.add_argument("-p", "--preprocess", action="store_true", default=False, help="Preprocessar imagem 'model.preprocess_input(...)'")
 parser.add_argument("-t", "--trainable", action="store_true", default=False, help="Define se terá as camadas do modelo de transfer-learning treináveis ou não")
 parser.add_argument("-T", "--Test", action="store_true", default=False, help="Define execução apenas para o teste")
-parser.add_argument("-e", "--epochs", action="count", default=100, type=int, help="Quantidade de épocas para o treino")
-parser.add_argument("-P", "--patience", action="count", default=5, type=int, help="Quantidade de paciência no early stopping")
+parser.add_argument("-e", "--epochs", action="count", default=100, help="Quantidade de épocas para o treino")
+parser.add_argument("-P", "--patience", action="count", default=5, help="Quantidade de paciência no early stopping")
 
 args = parser.parse_args()
 
