8200c5b194dc0f889fa589c124db7f0ac36b4228
julianorinaldi <julianorinaldi@gmail.com>
2023-10-02 10:26:26 -0300

    FIX Abstract Base Class + Liner Regression

--- models/modelLinearRegression.py
+++ models/modelLinearRegression.py
@@ -1,7 +1,11 @@
 from sklearn.linear_model import LinearRegression
+from entityModelConfig import ModelConfig
 from modelABCRegressor import ModelABCRegression
 
 class ModelLinearRegression(ModelABCRegression):
     
+    def __init__(self, modelConfig : ModelConfig):
+        super().__init__(modelConfig)
+        
     def getSpecialistModel(self):
         return LinearRegression()
