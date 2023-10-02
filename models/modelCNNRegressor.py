8ae7e88114bd7cfd96664a504cad52215fc0a0f5
julianorinaldi <julianorinaldi@gmail.com>
2023-10-02 13:55:00 -0300

    Reestruturação geral

--- models/modelCNNRegressor.py
+++ models/modelCNNRegressor.py
@@ -22,7 +22,6 @@ class ModelRegressorCNN(ModelABCRegressor):
                     ])
 
         opt = tf.keras.optimizers.RMSprop()
-
         _model.compile(optimizer=opt, loss='mse', metrics=['mae', 'mse'])
 
         _model.summary()
@@ -32,9 +31,13 @@ class ModelRegressorCNN(ModelABCRegressor):
         return X
     
     def modelFit(self, model, X_, Y_carbono):
-        model.fit(X_, Y_carbono, validation_split=0.3, epochs=self.modelConfig.argsEpochs, 
-                            callbacks=[tf.keras.callbacks.EarlyStopping
-                                    (monitor='val_loss', patience=self.modelConfig.argsPatience, restore_best_weights=True)])
+        earlyStopping = tf.keras.callbacks.EarlyStopping(
+                monitor='val_loss', patience=self.modelConfig.argsPatience, 
+                    restore_best_weights=True)
+        
+        model.fit(X_, Y_carbono, validation_split=0.3, 
+                    epochs=self.modelConfig.argsEpochs, 
+                            callbacks=[earlyStopping])
         
         model.save(filepath=self.modelConfig.argsNameModel, save_format='tf', overwrite=True)
         print(f"{self.modelConfig.printPrefix} Model Saved!!!")
