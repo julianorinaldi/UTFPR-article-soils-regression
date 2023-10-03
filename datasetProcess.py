f75ef50b469da092ae08dc16f4fea764f16fd056
julianorinaldi <julianorinaldi@gmail.com>
2023-10-02 21:59:43 -0300

    Normalização MinMaxScaler

--- datasetProcess.py
+++ datasetProcess.py
@@ -1,6 +1,6 @@
 
 import pandas as pd  # Trabalhar com análise de dados, importação, etc.
-
+from sklearn import preprocessing
 from entityModelConfig import ModelConfig
 
 # Faz o preparado do Dataset para trabalhar no modelo de regressão
@@ -28,9 +28,14 @@ def dataset_process(modeConfig : ModelConfig):
     df = df.drop(columns={"arquivo"})
     
     # Normalização Dataset Treinamento
-    df_stats = df.describe()
-    df_stats = df_stats.transpose()
-    df = (df - df_stats['mean']) / df_stats['std']
+    # df_stats = df.describe()
+    # df_stats = df_stats.transpose()
+    # df = (df - df_stats['mean']) / df_stats['std']
 
+    # Normalização MinMaxScaler
+    x = df.values
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df = pd.DataFrame(x_scaled)
     
     return df, imagefiles
