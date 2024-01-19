280641a845824143ec1e7d3e0811f15ca4ab4f00
julianorinaldi <julianorinaldi@gmail.com>
2024-01-19 10:03:47 -0300

    Teste Log

--- log/LoggingPy.py
+++ log/LoggingPy.py
@@ -2,10 +2,8 @@ import logging
 from datetime import datetime, timezone, timedelta
 
 class LoggingPy:
-    LOGNAME = "Default"
     def __init__(self, nameModel : str = "emptyNameModel", prefix : str = ">>>>>>>>>>>>>>>>>", log_level : int = 1):
         # Configurar o nível de log (pode ser logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, ou logging.CRITICAL)
-        print(f"********************** Instanciou Log uma vez {nameModel} **********************")
         self.logger : logging.Logger
         
         if log_level == 0:
@@ -21,32 +19,29 @@ class LoggingPy:
         else:
             logLevel=logging.DEBUG
         
-        logging.basicConfig(level=logLevel)
+        # Criar e configurar o logger
+        self.logger = logging.getLogger(nameModel)
+        self.logger.setLevel(logLevel)
 
         tz_utc_minus3 = timezone(timedelta(hours=-3))
         timestamp = datetime.now(tz=tz_utc_minus3).strftime("%Y%m%d_%H%M%S")
-        log_filename = f'log_{nameModel}_{timestamp}.log'
-        
-        # Configurar um manipulador de console
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logLevel)
-        formatter = logging.Formatter(f'%(asctime)s - %(levelname)s {prefix} %(message)s')
-
-        # Configurar o fuso horário para UTC-3
-        tz_utc_minus3 = timezone(timedelta(hours=-3))
-        formatter.converter = lambda *args: datetime.now(tz=tz_utc_minus3).timetuple()
-
-        console_handler.setFormatter(formatter)
+        log_filename = f'logs/log_{nameModel}_{timestamp}.log'
 
         # Configurar um manipulador de arquivo
         file_handler = logging.FileHandler(log_filename)
-        file_handler.setLevel(logLevel)
-        file_handler.setFormatter(formatter)
-
-        # Criar e configurar o logger
-        self.logger = logging.getLogger(LoggingPy.LOGNAME)
-        #self.logger.addHandler(console_handler)
+        formatterFile = logging.Formatter(f'%(asctime)s - %(levelname)s %(message)s')
+        formatterFile.converter = lambda *args: datetime.now(tz=tz_utc_minus3).timetuple()
+        file_handler.setFormatter(formatterFile)
         self.logger.addHandler(file_handler)
+
+        
+        # Configurar um manipulador de console
+        console_handler = logging.StreamHandler()
+        formatterStream = logging.Formatter(f'%(asctime)s - %(levelname)s {prefix} %(message)s')
+        formatterStream.converter = lambda *args: datetime.now(tz=tz_utc_minus3).timetuple()
+        console_handler.setFormatter(formatterStream)
+        self.logger.addHandler(console_handler)
+        
 
     def logDebug(self, message):
         self.logger.debug(message)
