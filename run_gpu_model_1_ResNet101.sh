26799e81d2c74f2d3855b79766bf8a3df50f8ea3
julianorinaldi-TUF <julianorinaldi@gmail.com>
2024-07-02 13:36:22 -0300

    Script Novos Modelos

---
+++ run_gpu_model_1_ResNet101.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+python3 main.py --name ResNet101_1_Normalize0_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 0 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+python3 main.py --name ResNet101_1_Normalize1_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 1 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+python3 main.py --name ResNet101_1_Normalize2_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 2 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+python3 main.py --name ResNet101_1_Normalize3_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 3 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+python3 main.py --name ResNet101_1_Normalize4_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 3 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+# Sem o preprocess
+python3 main.py --name ResNet101_1_Normalize0_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 0 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+
+python3 main.py --name ResNet101_1_Normalize1_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 1 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+
+python3 main.py --name ResNet101_1_Normalize2_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 2 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+
+python3 main.py --name ResNet101_1_Normalize3_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 3 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
+
+python3 main.py --name ResNet101_1_Normalize4_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 3 \
+               --log_level 0 \
+               --model 1 \
+               --epochs 100 \
+               --patience 10 \
---
+++ run_gpu_model_20_EfficientNetB7.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+python3 main.py --name EfficientNetB7_20_Normalize0_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 0 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+python3 main.py --name EfficientNetB7_20_Normalize1_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 1 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+python3 main.py --name EfficientNetB7_20_Normalize2_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 2 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+python3 main.py --name EfficientNetB7_20_Normalize3_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 3 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+python3 main.py --name EfficientNetB7_20_Normalize4_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 3 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+               --preprocess
+
+# Sem o preprocess
+python3 main.py --name EfficientNetB7_20_Normalize0_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 0 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+
+python3 main.py --name EfficientNetB7_20_Normalize1_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 1 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+
+python3 main.py --name EfficientNetB7_20_Normalize2_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 2 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+
+python3 main.py --name EfficientNetB7_20_Normalize3_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 3 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
+
+python3 main.py --name EfficientNetB7_20_Normalize4_Epochs100_Patience10_Separed_Preprocess \
+               --normalize 3 \
+               --log_level 0 \
+               --model 20 \
+               --epochs 100 \
+               --patience 10 \
