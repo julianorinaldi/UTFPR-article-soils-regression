e3181853f1a3da53675e14714b6775bc533e26c3
julianorinaldi-TUF <julianorinaldi@gmail.com>
2024-07-03 19:30:48 -0300

    Organização

--- run_gpu_model_20_EfficientNetB7.sh
+++ run_gpu_model_20_EfficientNetB7.sh
@@ -41,35 +41,35 @@ python3 main.py --name EfficientNetB7_20_Normalize4_Epochs100_Patience10_Separed
                --preprocess
 
 # Sem o preprocess
-python3 main.py --name EfficientNetB7_20_Normalize0_Epochs100_Patience10_Separed_Preprocess \
+python3 main.py --name EfficientNetB7_20_Normalize0_Epochs100_Patience10_Separed \
                --normalize 0 \
                --log_level 0 \
                --model 20 \
                --epochs 100 \
                --patience 10 \
 
-python3 main.py --name EfficientNetB7_20_Normalize1_Epochs100_Patience10_Separed_Preprocess \
+python3 main.py --name EfficientNetB7_20_Normalize1_Epochs100_Patience10_Separed \
                --normalize 1 \
                --log_level 0 \
                --model 20 \
                --epochs 100 \
                --patience 10 \
 
-python3 main.py --name EfficientNetB7_20_Normalize2_Epochs100_Patience10_Separed_Preprocess \
+python3 main.py --name EfficientNetB7_20_Normalize2_Epochs100_Patience10_Separed \
                --normalize 2 \
                --log_level 0 \
                --model 20 \
                --epochs 100 \
                --patience 10 \
 
-python3 main.py --name EfficientNetB7_20_Normalize3_Epochs100_Patience10_Separed_Preprocess \
+python3 main.py --name EfficientNetB7_20_Normalize3_Epochs100_Patience10_Separed \
                --normalize 3 \
                --log_level 0 \
                --model 20 \
                --epochs 100 \
                --patience 10 \
 
-python3 main.py --name EfficientNetB7_20_Normalize4_Epochs100_Patience10_Separed_Preprocess \
+python3 main.py --name EfficientNetB7_20_Normalize4_Epochs100_Patience10_Separed \
                --normalize 3 \
                --log_level 0 \
                --model 20 \
