c4729f682a0fe534fa7b0d037986c434f94d1e1d
julianorinaldi-TUF <julianorinaldi@gmail.com>
2024-07-08 23:02:29 -0300

    Test Final

--- run_test_TEST.sh
+++ run_test_TEST.sh
@@ -3,7 +3,6 @@
 python3 test-model.py --name TF_EfficientNetV2S_21_Normalize0_Epochs300_Patience20_20240707_2104.h5 \
                --model 21 \
                --log_level 0 \
-               --amount_image_test 50 \
-               --preprocess \
+               --amount_image_test 3843 \
                --show_model
 
